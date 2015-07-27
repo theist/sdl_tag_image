e170c7de15637bf6466229db5a7dd2f1a87b2c2e
Carlos Peñas <carlos.penas@the-cocktail.com>
2015-07-27 18:51:17 +0200

    basic rubygametemplate

---
+++ quicktag.rb
@@ -0,0 +1,40 @@
+#!/bin/env ruby
+
+require 'rubygems'
+require 'rubygame'
+
+class Game
+  def initialize
+    @screen = Rubygame::Screen.new [640,480], 0, [Rubygame::HWSURFACE, Rubygame::DOUBLEBUF]
+    @screen.title = "Generic Game!"
+
+    @queue = Rubygame::EventQueue.new
+    @clock = Rubygame::Clock.new
+    @clock.target_framerate = 30
+  end
+
+  def run
+    loop do
+      update
+      draw
+      @clock.tick
+    end
+  end
+
+  def update
+    @queue.each do |ev|
+      case ev
+        when Rubygame::QuitEvent
+          Rubygame.quit
+          exit
+      end
+    end
+  end
+
+  def draw
+    @screen.update
+  end
+end
+
+game = Game.new
+game.run
