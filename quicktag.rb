0aa66145468c1f67467fb4e198cb113aee2aef00
Carlos Peñas <carlos.penas@the-cocktail.com>
2015-07-27 23:34:30 +0200

    quick hud

--- quicktag.rb
+++ quicktag.rb
@@ -3,10 +3,55 @@
 require 'rubygems'
 require 'rubygame'
 
+class Hud
+
+  # construct the HUD
+  def initialize options
+    @screen = options[:screen]  # need that to blit on it
+    @game = options[:game]
+
+    # TTF.setup must be called before any font object is created
+    Rubygame::TTF.setup
+
+    # point to the TTF file
+    filename = File.join(File.dirname(__FILE__), 'verdana.ttf')
+
+    # creates the font object, which is used for drawing text
+    @cosmic_font = Rubygame::TTF.new filename, 24
+
+    # initialize options that are displayed, here time
+    @time = "-"
+
+  end
+
+  # called from the game class in each loop. updates options that are displayed
+  def update options
+    @time = options[:time]
+  end
+
+  # called from the game class in the draw method. render any options 
+  # and blit the surface on the screen
+  def draw
+    ypos = 6
+    @game.categories.each do |category|
+      string = @cosmic_font.render(category ,true,[255,255,255],[0,0,0])
+      string.blit @screen, [@screen.w-string.w-6, ypos]
+      ypos = ypos + string.h + 6
+    end
+  end
+
+end
+
+
+
 class Game
+
+  attr_accessor :categories
+
   def initialize
     @screen = Rubygame::Screen.new [640,480], 0, [Rubygame::HWSURFACE, Rubygame::DOUBLEBUF]
     @screen.title = "Generic Game!"
+    @hud = Hud.new :screen => @screen, :game => self
 
     @queue = Rubygame::EventQueue.new
     @clock = Rubygame::Clock.new
@@ -68,6 +113,8 @@ class Game
   end
 
   def draw
+    @screen.fill :black
+    @hud.draw
     @screen.update
   end
 end
